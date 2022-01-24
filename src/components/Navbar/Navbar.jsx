c04236f35f27fec0ee3835ca4284547bc6371676
FIRST_NAME LAST_NAME <rabbimovxumoyun22@gmail.com>
2022-01-24 19:51:14 +0500

    navbar to'girlandi

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ()=>{
          <div className="row ">
              <div className="col-xl-3 col-lg-3 d-flex pb-4 pb-md-0">
                  <div className="logo">
-                 <img src = "./assets/images/svg/Expedia.svg"></img>
+                 <img src = "./assets/images/svg/Expedia.svg" alt = "error"></img>
                  </div>
                  
                  <button className="form-control d-lg-none  btnSign">Sign Up</button>
@@ -17,20 +17,20 @@ const Navbar = ()=>{
              <div className="col-lg-6 d-flex justify-content-center   " >
                      <ul className=" nav">
                          <li className =" nav-item">
-                             <a className="nav-link active"  > Home</a>
+                             <a className="nav-link active" href="/home"  > Home</a>
                          </li>
                         
                          <li className =" nav-item">
-                             <a className="nav-link " > Trops</a>
+                             <a className="nav-link " href = "/trops" > Trops</a>
                          </li>
                          <li className =" nav-item">
-                             <a className="nav-link " > About</a>
+                             <a className="nav-link " href="/about" > About</a>
                          </li>
                          <li className =" nav-item">
-                             <a className="nav-link " >Support</a>
+                             <a className="nav-link " href="/support" >Support</a>
                          </li>
                          <li className =" nav-item">
-                             <a className="nav-link " >Travel</a>
+                             <a className="nav-link " href="/travel" >Travel</a>
                          </li>
                      </ul>
              </div>
